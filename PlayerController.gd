4ed2470689a3ca32bd3d89b024f9f9eebe348702
mjharrison <mjsonofharry@gmail.com>
2018-12-10 19:00:29 -0500

    dodge move and general improvements

--- PlayerController.gd
+++ PlayerController.gd
@@ -3,10 +3,12 @@ extends KinematicBody
 
 const SPEED = 6
 const WALK_SPEED = 2
+const JUMP_SPEED = 4
+const DODGE_FLOOR_SPEED = 10
+const DODGE_VERTICAL_SPEED = 2
+const GRAVITY = -9.8
 const ACCELERATION = 10
 const DEACCELERATION = 10
-const GRAVITY = -9.8
-const JUMP_SPEED = 3
 const LOOK_SENSITIVITY = 0.1
 
 
@@ -20,30 +22,38 @@ func _ready():
 
 
 func _process(delta):
-	var camera_basis = get_node("yaw/camera").get_global_transform().basis
+	var camera_transform = get_node("yaw/camera").get_global_transform()
 	var direction = Vector3()
 	if Input.is_action_pressed("move_forward"):
-		direction -= camera_basis[2]
+		direction -= camera_transform.basis[2]
 	if Input.is_action_pressed("move_backward"):
-		direction += camera_basis[2]
+		direction += camera_transform.basis[2]
 	if Input.is_action_pressed("move_left"):
-		direction -= camera_basis[0]
+		direction -= camera_transform.basis[0]
 	if Input.is_action_pressed("move_right"):
-		direction += camera_basis[0]
+		direction += camera_transform.basis[0]
 	direction.y = 0
-	direction = direction.normalized()
+	var direction_normal = direction.normalized()
 	
-	var target = direction * (WALK_SPEED if Input.is_action_pressed("walk") else SPEED)
+	var target = direction_normal * (
+		WALK_SPEED if Input.is_action_pressed("walk") else SPEED
+	) * (
+		DODGE_FLOOR_SPEED if Input.is_action_pressed("dodge") and is_on_floor() else 1
+	)
 	var floor_velocity = Vector3(m_velocity.x, 0, m_velocity.z)
-	var acceleration = ACCELERATION if direction.dot(floor_velocity) > 0 else DEACCELERATION
+	var acceleration = ACCELERATION if direction_normal.dot(floor_velocity) > 0 else DEACCELERATION
 	floor_velocity = floor_velocity.linear_interpolate(target, acceleration * delta)
 
 	m_velocity.x = floor_velocity.x
 	m_velocity.z = floor_velocity.z
 	m_velocity.y += delta * GRAVITY
 	m_velocity = move_and_slide(m_velocity, Vector3(0, 1, 0))
-	if (is_on_floor() and Input.is_action_pressed("jump")):
-		m_velocity.y = JUMP_SPEED
+	
+	if is_on_floor():
+		if Input.is_action_pressed("jump"):
+			m_velocity.y = JUMP_SPEED
+		elif Input.is_action_pressed("dodge"):
+			m_velocity.y = DODGE_VERTICAL_SPEED
 
 
 func _input(ie):
